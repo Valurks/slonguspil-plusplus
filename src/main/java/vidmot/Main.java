3af8478f3adf52d8d175f5bd0ef7869422d76a24
Hjollsari <hjorleifursveins@gmail.com>
2025-04-12 18:55:08 +0000

    Jar file implemented

---
+++ src/main/java/vidmot/Main.java
@@ -0,0 +1,12 @@
+package vidmot;
+
+/**
+ * Nafn: Hjörleifur Örn Sveinsson
+ * Gmail: hjorleifursveins@gmail.com
+ * Lýsing:
+ */
+public class Main {
+    public static void main(String[] args) {
+        SlangaApplication.main(args);
+    }
+}
